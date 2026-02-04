c8d5bdfd39246042183d47c4e7f340fbe0de0531
zetzehr <j.abasolo.agenjo@gmail.com>
2026-02-04 10:52:06 +0100

    readme creado

--- src/modules/keyword/keyword.service.ts
+++ src/modules/keyword/keyword.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Keyword, KeywordSchema } from './keyword.schema';
+import { Keyword } from './keyword.schema';
 
 @Injectable()
 export class KeywordService {
--- src/modules/offer/offer.schema.ts
+++ src/modules/offer/offer.schema.ts
@@ -1,4 +1,3 @@
-// offer.schema.ts
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
